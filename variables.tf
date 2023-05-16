0e11a2cb307d3a728cd41dc0dc81d9f8628fcad6
subhashgaurav <91779017+subhashgaurav@users.noreply.github.com>
2023-05-16 09:38:37 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,35 @@
+variable "rg_name" {
+  type = string 
+}
+
+variable "location" {
+  type = string
+}
+
+variable "vNet_name" {
+  type = string 
+}
+
+variable "subnet_name"
+type = string 
+}
+
+variable "pulic_ip_name" {
+  type = string 
+}
+
+variable "nic_name" {
+  type = string 
+}
+
+variable "vm_name" {
+  type = string
+}
+
+variable "os_disk_name" {
+  type = string 
+}
+
+variable "data_disk_name" {
+  type = string 
+}
